e542f6f26806b6cd2af81785771df0c0c06552d5
igor.gucaev <igor.gucaev@gmail.com>
2019-05-26 22:41:06 +0300

    scheduler implemented

--- Scheduler/libraries/Abstractions/Models/Task.cs
+++ Scheduler/libraries/Abstractions/Models/Task.cs
@@ -9,26 +9,26 @@ namespace Scaffolding.Scheduler.Abstractions
         /// <summary>
         /// Unique task identifier
         /// </summary>
-        public string Identifier { get; private set; }
-        public string Title { get; private set; }
-        public string Description { get; private set; }
+        public string Identifier { get; protected set; }
+        public string Title { get; protected set; }
+        public string Description { get; protected set; }
         /// <summary>
         /// Notification settings
         /// </summary>
-        public string Notification { get; private set; }
+        public string Notification { get; protected set; }
         /// <summary>
         /// Run configuration
         /// </summary>
-        public string Schedule { get; private set; }
-        public TimeSpan Horizon { get; private set; } // Plan task til Now + Horizon
+        public string Schedule { get; protected set; }
+        public TimeSpan Horizon { get; protected set; } // Plan task til Now + Horizon
         /// <summary>
         /// Task input
         /// </summary>
-        public string Arguments { get; private set; }
+        public string Arguments { get; protected set; }
 
-        public string Agents { get; private set; }
+        public string Agents { get; protected set; }
 
-        public bool Active { get; private set; }
-        public TimeSpan Timeout { get; private set; }
+        public bool Active { get; protected set; }
+        public TimeSpan Timeout { get; protected set; }
     }
 }
--- Tests/Scheduler/ConstructionTest.cs
+++ Tests/Scheduler/ConstructionTest.cs
@@ -1,17 +1,22 @@
+using Newtonsoft.Json;
 using Scaffolding.Scheduler.Core;
+using Scaffolding.Tests.Scheduler.Models;
 using System;
 using System.Linq;
 using System.Threading;
 using Xunit;
 
-namespace Scheduler
+namespace Scaffolding.Tests.Scheduler
 {
     public class SchedulerTest
     {
         [Theory]
-        [MemberData()]
-        public void Test_Scheduler_Build()
+        [InlineData("report", "{\"day\": { \"mode\": 1, \"args\": \"\" },\"time\": { \"mode\": 1, \"args\": {\"Item1\":\"09:00:00\",\"Item2\":\"00:45:00\"} }}", "", 24)]
+        public void Test_Scheduler_Build(string identifier, string schedule, string agents, double horizonHours)
         {
+            //string d = JsonConvert.SerializeObject(new ValueTuple<TimeSpan, TimeSpan>(new TimeSpan(9, 0, 0), new TimeSpan(0, 45, 0)));
+
+
             // Prepare
             Scaffolding.Scheduler.Core.Scheduler scheduler =
                 new Scaffolding.Scheduler.Core.Scheduler((settings) =>
@@ -19,10 +24,10 @@ namespace Scheduler
                     settings.DueTime = new TimeSpan(0, 0, 5);
                     settings.Period = new TimeSpan(0, 5, 0);
                     settings.Tasks = // DataProvider.GetTasks
-                   (new Scaffolding.Scheduler.Abstractions.Task[0]
-                   {
-                       Task.
-                   }).ToList();
+                    (new Scaffolding.Scheduler.Abstractions.Task[]
+                    {
+                        TestTask.Create(identifier, schedule, string.Empty, agents, TimeSpan.FromHours(horizonHours))
+                    }).ToList();
                 }, (job) => true);
 
             // Pre-validate
