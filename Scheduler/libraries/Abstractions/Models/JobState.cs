e340173043ba0494ba8f7775196e044b08260a75
Igor Gutsaev <igor.gutsaev@filuet.ru>
2020-08-20 21:40:44 +0300

    Job state update

--- Scheduler/libraries/Abstractions/Models/JobState.cs
+++ Scheduler/libraries/Abstractions/Models/JobState.cs
@@ -1,24 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Scaffolding.Scheduler.Abstractions
 {
     public enum JobState
     {
+        [Description("Created")]
         Created,
+        [Description("InProgress")]
         InProgress,
         /// <summary>
         /// Done successfully
         /// </summary>
+        [Description("Done")]
         Done,
         /// <summary>
         /// Failed
         /// </summary>
+        [Description("Failed")]
         Failed,
         /// <summary>
         /// Closed manually
         /// </summary>
+        [Description("Closed")]
         Closed
     }
 }
